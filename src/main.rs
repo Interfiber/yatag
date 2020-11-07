6e3bd76a09ece7027629409e449d8255eb71a969
Interfiber <stasonis@gmail.com>
2020-11-07 14:46:50 -0500

    Adds text system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+pub fn get_input(prompt: &str) -> String{
+    println!("{}",prompt);
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_goes_into_input_above) => {},
+        Err(_no_updates_is_fine) => {},
+    }
+    input.trim().to_string()
+}
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let cmd: String = get_input(">");
+        if cmd == "exit" {
+            std::process::exit(1);
+        }
+        if cmd == "items" {
+            
+        }
+
+    }
 }
